0b85b539305f38f61be5f235a288bb356962c31f
Taruna Patel <pateltaruna552@gmail.com>
2023-11-28 13:10:11 +0530

    purchase and historical model added

--- Vendors/models.py
+++ Vendors/models.py
@@ -1,10 +1,20 @@
 from django.db import models
-from phonenumber_field.modelfields import PhoneNumberField
+
 # Create your models here.
 
 
 class Vendor(models.Model):
-    name = models.CharField(max_length=100)
-    contact_detils = PhoneNumberField()
-    address = models.CharField(max_length=100)
-    vendor_code = models.CharField(max_length=50 , unique= True)
+    name = models.CharField(max_length=250)
+    contact_details = models.TextField(max_length=250)
+    address = models.TextField(max_length=250)
+    vendor_code = models.CharField(max_length=250)
+    on_time_delivery_rate = models.FloatField(null=True, blank=True)
+    quality_rating_avg = models.FloatField(null=True, blank=True)
+    average_response_time = models.FloatField(null=True, blank=True)
+    fulfillment_rate = models.FloatField(null=True, blank=True)
+
+
+
+
+    def __str__(self):
+        return self.name
--- vendors_management/settings.py
+++ vendors_management/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'vendors',
+    'purchase_order',
+    'historical_performance'
 ]
 
 MIDDLEWARE = [
