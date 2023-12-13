b3b5bba5007056d0d85cb97b53a3cdaad2604c07
kelvinmacharia254 <pilotkelvin0@gmail.com>
2023-12-13 16:44:04 +0300

    feat(user): Extend Django User model and updated dependent models. 	1. Extend the User model to included contacts and address field for users. 	2. Delete all redudant fields from the Vendor model. 	3. Refactor Historicalperformance model to VendorPerformance to capture raw performance data and not aggregates. 	4. Include a new model VendorPerformanceAverages to store aggregates which will be computed dynamically using signals 	   anytime there are changes in the VendorPerformance model.

--- Vendors/admin.py
+++ Vendors/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Vendor , PurchaseOrder , HistoricalPerformance
+from .models import Vendor , PurchaseOrder , VendorPerformance
 # Register your models here.
 admin.site.register(Vendor)
 admin.site.register(PurchaseOrder)
-admin.site.register(HistoricalPerformance)
+admin.site.register(VendorPerformance)
